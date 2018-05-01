3ae997535d8f0f82f732677629d876bf70c4d2a9
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-05-01 10:05:08 +0300

    [~] Extract app listen call into a separate file

--- src/app/index.js
+++ src/app/index.js
@@ -7,7 +7,6 @@ const middleware = require('./middleware');
 const rootRouter = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 3030;
 
 app.disable('x-powered-by');
 
@@ -17,7 +16,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', rootRouter);
 app.use(middleware.notFound);
 app.use(middleware.error);
-
-app.listen(port, () => console.info(`Example app listening on port ${port}!`));
 
 module.exports = app;
